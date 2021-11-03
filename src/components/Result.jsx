bfb423c87e6f3e215c48a7923fe7c53b87ef2b00
BryanGrados <i202122238@cibertec.edu.pe>
2021-11-03 11:22:57 -0500

    -

---
+++ src/components/Result.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Result = (props) => {
+
+    console.log('Result render', props)
+    return (
+        <div className="result">
+            <span>0</span>
+        </div>
+    )
+}
+
+export default Result
